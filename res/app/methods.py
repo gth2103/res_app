1bcea162e9ad7842ffbeeceef5a8c8717324469b
Grant <gth2103@columbia.edu>
2019-10-03 19:18:20 -0400

    test print first item in file content in

--- res/app/methods.py
+++ res/app/methods.py
@@ -61,6 +61,7 @@ def add_to_list(item_list, item, index):
 	file_in = open("/home/grant/res_app/res/seed.py", "r")
 	file_content_in = file_in.readlines()
 	file_in.close()
+	print(file_content_in[0])
 	file_out = open("/home/grant/res_app/res/seed.txt", "w")
 	file_content_out = "".join(file_content_in)
 	file_out.write(file_content_out)
