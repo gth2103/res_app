7ccff38dbef02595dbb633acc5f49866b9894270
Grant <gth2103@columbia.edu>
2019-10-03 18:28:22 -0400

    test write to file

--- res/app/methods.py
+++ res/app/methods.py
@@ -58,10 +58,13 @@ def get_index(list):
     return len(list)
 
 def add_to_list(item_list, item, index):
-	file = open("/home/grant/res_app/res/app/seed.py", "r")
-	file_list = file.readlines()
-	file.close()
-	print(file_list)
+	file_in = open("/home/grant/res_app/res/app/seed.py", "r")
+	file_list = file_in.readlines()
+    file_in.close()
+	file_out = open("/home/grant/res_app/res/app/seed.txt", "w")
+	file_out.write(file_list)
+	file_out.close()
+
 	if item not in item_list:
 		if int(index) < len(item_list):
 			item_list.pop(int(index))
