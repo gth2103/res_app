ebc786d7a6bfb9238368bcdcfd39b77b5026ec20
Grant <gth2103@columbia.edu>
2019-10-03 18:30:40 -0400

    indent error

--- res/app/methods.py
+++ res/app/methods.py
@@ -60,7 +60,7 @@ def get_index(list):
 def add_to_list(item_list, item, index):
 	file_in = open("/home/grant/res_app/res/app/seed.py", "r")
 	file_list = file_in.readlines()
-    file_in.close()
+	file_in.close()
     file_out = open("/home/grant/res_app/res/app/seed.txt", "w")
 	file_out.write(file_list)
 	file_out.close()
