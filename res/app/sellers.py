1aa46ff27158dd3a60ac8f409e41365ffa369a6e
Grant <gth2103@columbia.edu>
2019-10-04 01:50:21 -0400

    testing

--- res/app/sellers.py
+++ res/app/sellers.py
@@ -7,10 +7,9 @@ sellers_index = 0
 def initiate_sellers():
 	print(users)
 	for user in users:
-    	for key, value  in user.items():
-    		if key == 'items_list':
-    			if value:
-    				for key, value in user.items():
-    					if key == 'user_id':
-    						print(value)
-    						add_to_list("sellers", sellers, value, len(sellers))
+		for key, value  in user.items():
+			if key == 'items_list':
+				if value:
+					for key, value in user.items():
+						if key == 'user_id':
+							add_to_list("sellers", sellers, value, len(sellers))
