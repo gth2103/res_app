63dbfec8f7fb7ea10d8ed646e0cecc9e4c3d14f3
Grant <gth2103@columbia.edu>
2019-10-03 23:24:42 -0400

    changed write to writelines in write_data

--- res/app/methods.py
+++ res/app/methods.py
@@ -48,20 +48,20 @@ def write_data(data_name, data_in):
 	file_out = open("/home/grant/res_app/res/data/" + data_name + ".txt", "a")
 	if(type(data_in) is dict):
 		file_content_out = json.dumps(data_in)
-		file_out.write(file_content_out)
+		file_out.writelines(file_content_out)
 	elif(type(data_in) is list):
 		file_content_out = "".join(str(value) for value in data_in)
-		file_out.write(file_content_out)
+		file_out.writelines(file_content_out)
 	file_out.close()
 
 def over_write_data(data_name, data_in):
 	file_out = open("/home/grant/res_app/res/data/" + data_name + ".txt", "w")
 	if(type(data_in) is dict):
 		file_content_out = json.dumps(data_in)
-		file_out.write(file_content_out)
+		file_out.writeline(file_content_out)
 	elif(type(data_in) is list):
 		file_content_out = "".join(str(value) for value in data_in)
-		file_out.write(file_content_out)
+		file_out.writelines(file_content_out)
 	file_out.close()
 
 def search(list_name, search_list_out, search_list_in):
